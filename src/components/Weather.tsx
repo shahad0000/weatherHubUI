689261a170877f1f471ba891754ddf45511bbd59
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-06-22 23:20:27 +0300

    add loading..

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -5,38 +5,42 @@ const BASE_URL = "https://weatherhubapi.onrender.com";
 
 const Weather = () => {
   const [weather, setWeather] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   const getWeather = async () => {
     try {
-      setLoading(true);
       const res = await axios.get(`${BASE_URL}/weather?lat=24.71&lon=46.68`, {
         withCredentials: true,
       });
       setWeather(res.data);
     } catch (err: any) {
-      setError(err.message || "Failed to fetch weather");
-    } finally {
-      setLoading(false);
+      console.error(err);
     }
   };
   useEffect(() => {
     getWeather();
   }, []);
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+
+
+  if (
+    !weather ||
+    !weather.main ||
+    !weather.weather ||
+    !weather.weather[0] ||
+    !weather.wind
+  ) {
+    return <div>Loading...</div>;
+  }  
 
   return (
     <div>
-      {weather ? (
+      {weather && (
         <div>
           <h2>Weather in {weather.name || "Unknown Location"}</h2>
           <p>
             <strong>Temperature:</strong>
             {weather.main?.temp !== undefined
               ? (weather.main.temp - 273.15).toFixed(1)
-              : "N/A"}{" "}
+              : "N/A"}
             °C
           </p>
           <p>
@@ -54,8 +58,6 @@ const Weather = () => {
               : "N/A"}
           </p>
         </div>
-      ) : (
-        <div>Loading....</div>
       )}
     </div>
   );
