407ce64d68199a9a2f04c3c4cd81e4f02864ad53
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-06-22 18:35:31 +0300

    first commit

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,24 +1,15 @@
-import React from 'react'
-import axios from 'axios';
+import axios from "axios";
 
-    const BASEURL = "https://weatherhubapi.onrender.com/";
+const BASEURL = "https://weatherhubapi.onrender.com/";
 
 const getWeather = async () => {
-    const res = await axios.get(`${BASEURL}weather?lat=24.71&lon=46.68`);
-    console.log(res.data)
+  const res = await axios.get(`${BASEURL}weather?lat=24.71&lon=46.68`);
+  console.log(res.data);
 };
 getWeather();
 
-
 const Weather = () => {
+  return <div>This is the weather page</div>;
+};
 
-
-
-  return (
-    <div>
-        This is the weather page
-    </div>
-  )
-}
-
-export default Weather
+export default Weather;
