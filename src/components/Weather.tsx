8dbd48a6e48a155c6b550b90f651bc1226c7430b
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-06-22 22:33:30 +0300

    add loading..

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -5,16 +5,28 @@ const BASE_URL = "https://weatherhubapi.onrender.com";
 
 const Weather = () => {
   const [weather, setWeather] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   const getWeather = async () => {
-    const res = await axios.get(`${BASE_URL}/weather?lat=24.71&lon=46.68`, {
-      withCredentials: true,
-    });
-    console.log(res.data);
-    setWeather(res.data);
+    try {
+      setLoading(true);
+      const res = await axios.get(`${BASE_URL}/weather?lat=24.71&lon=46.68`, {
+        withCredentials: true,
+      });
+      setWeather(res.data);
+    } catch (err: any) {
+      setError(err.message || "Failed to fetch weather");
+    } finally {
+      setLoading(false);
+    }
   };
   useEffect(() => {
     getWeather();
   }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div>
       {weather ? (
