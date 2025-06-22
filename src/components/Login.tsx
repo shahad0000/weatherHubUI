f3cfa2f8522621fbe1a1abda97215b68a0d2fd2b
shahad0000 <81360310+shahad0000@users.noreply.github.com>
2025-06-23 02:49:39 +0300

    add logical and

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router";
 
 const BASE_URL = "https://weatherhubapi.onrender.com";
 
 const Login = () => {
-  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
